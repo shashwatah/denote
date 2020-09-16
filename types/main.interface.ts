527b3c8659d6f3a610eca0a5a813362970296351
KSSBro <shashwatdeadcliff@gmail.com>
2020-09-16 16:17:05 +0530

    update: main.interface.ts: added user interface; add: user.controller.ts, auth.route.ts

--- types/main.interface.ts
+++ types/main.interface.ts
@@ -3,3 +3,10 @@ export interface Note {
     title: string,
     description: string
 }
+
+export interface User {
+    id: string,
+    username: string,
+    password: string,
+    notes: Array<string>
+}
