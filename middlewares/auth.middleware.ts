5016ec29a6fca10181f7e406105b17108e7d954e
KSSBro <shashwatdeadcliff@gmail.com>
2020-09-17 19:38:06 +0530

    update: auth.middleware: fin, api.routes: added auth middlware to routes

---
+++ middlewares/auth.middleware.ts
@@ -0,0 +1,46 @@
+import { validateJwt, JwtValidation } from "https://deno.land/x/djwt/validate.ts";
+import { Payload } from "https://deno.land/x/djwt/create.ts";
+import { config } from 'https://deno.land/x/dotenv/mod.ts';
+
+const secret = config()["SECRET"];
+
+export const authenticate = async ({ request, response}: { request: any, response: any }, next: any) => {
+    const headers: Headers = await request.headers;
+
+    const authHeader = headers.get('Authorization');
+
+    if(!authHeader) {
+        response.status = 401;
+        response.body = {
+            message: "Unauthorized!",
+            status: 401
+        };
+        return;
+    }
+    
+    const jwt = authHeader.split(' ')[1];
+    if(!jwt) {
+        response.status = 401;
+        response.body = {
+            message: "Unauthorized!",
+            status: 401
+        };
+        return;
+    }
+
+    const validation: JwtValidation  = await validateJwt({ jwt, key: secret, algorithm: "HS256" });
+    if(validation.isValid) {
+        const payload: any =  validation.payload;
+        request.headers.set("user-id", payload.id);
+        await next();
+        return;
+    }
+
+    response.status = 401;
+    response.body = {
+        message: "Invalid Token.",
+        status: 401
+    };
+};
+
+
--- routes/api.routes.ts
+++ routes/api.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'https://deno.land/x/oak/mod.ts';
 import { notesController } from './../controllers/notes.controller.ts';
 import { usersController } from './../controllers/users.controller.ts';
 import { authController } from './../controllers/auth.controller.ts';
+import { authenticate } from './../middlewares/auth.middleware.ts';
 
 const router = new Router();
 
@@ -11,15 +12,15 @@ router.get("/", ({ response }: { response: any }) => {
 });
 
 router.get('/token', authController.getToken)
-    .get('/api/users', usersController.getUsers)
-    .post('/api/users/add', usersController.addUser)
-    .get('/api/users/:id', usersController.getUser)
-    .get('/api/users/:id/notes', usersController.getUserNotes)
-    .delete('/api/users/delete/:id', usersController.deleteUser)
-    .get('/api/notes', notesController.getNotes)
-    .get('/api/notes/:id', notesController.getNote)
-    .post('/api/notes/add', notesController.addNote)
-    .put('/api/notes/:id/update', notesController.updateNote)
-    .delete('/api/notes/delete/:id', notesController.deleteNote);
+    .get('/api/users', authenticate , usersController.getUsers)
+    .post('/api/users/add', authenticate, usersController.addUser)
+    .get('/api/users/:id', authenticate, usersController.getUser)
+    .get('/api/users/:id/notes', authenticate, usersController.getUserNotes)
+    .delete('/api/users/delete/:id', authenticate, usersController.deleteUser)
+    .get('/api/notes', authenticate, notesController.getNotes)
+    .get('/api/notes/:id', authenticate, notesController.getNote)
+    .post('/api/notes/add', authenticate, notesController.addNote)
+    .put('/api/notes/:id/update', authenticate, notesController.updateNote)
+    .delete('/api/notes/delete/:id', authenticate, notesController.deleteNote);
 
 export default router;
